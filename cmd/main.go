f929528c33482147fabaf33e1aad82beb8669d1b
mehmetcc <mehmetccm@gmail.com>
2025-05-17 20:23:20 +0300

    Add config.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mehmetcc/go-ems/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	config, err := config.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(config)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Application ApplicationConfig `mapstructure:"application"`
+}
+
+type ApplicationConfig struct {
+	BatchSize int `mapstructure:"batch-size"`
+}
+
+func LoadConfig() (*Config, error) {
+	viper.AddConfigPath("config") // look for config in the config directory
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, fmt.Errorf("config file not found: %w", err)
+		}
+		return nil, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	config := &Config{}
+	err := viper.Unmarshal(config)
+	if err != nil {
+		return nil, fmt.Errorf("unable to decode config into struct: %w", err)
+	}
+
+	return config, nil
+}
